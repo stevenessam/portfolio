96905412698246114aecb087f8b9c2de3d468b3c
Steven Essam <steven.essam.edwar@gmail.com>
2026-03-20 23:32:58 +0100

    Changed the structure to onepageprotfolio and created the rest 7 component

--- portfolio-frontend/src/app/app.routes.ts
+++ portfolio-frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { ProjectsComponent } from './components/projects/projects.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'projects', pathMatch: 'full' },
-  { path: 'projects', component: ProjectsComponent }
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' }
 ];
