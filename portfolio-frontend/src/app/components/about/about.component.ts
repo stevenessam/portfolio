c163c1061091baa7ac13e26856dc5a579cebe8bd
Steven Essam <steven.essam.edwar@gmail.com>
2026-03-21 21:48:32 +0100

    Angular About V1

--- portfolio-frontend/src/app/components/about/about.component.ts
+++ portfolio-frontend/src/app/components/about/about.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
