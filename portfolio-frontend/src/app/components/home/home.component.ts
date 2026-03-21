52ca1e5e94880700f7b628cfbc099e811fbbfbbf
Steven Essam <steven.essam.edwar@gmail.com>
2026-03-21 21:43:04 +0100

    Angular Home Page Setup V1

--- portfolio-frontend/src/app/components/home/home.component.ts
+++ portfolio-frontend/src/app/components/home/home.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../hero/hero.component';
+import { AboutComponent } from '../about/about.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { ExperienceComponent } from '../experience/experience.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent,
+    AboutComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
