c598937132de7fbf12363bed3c4a26f877a8979a
Steven Essam <steven.essam.edwar@gmail.com>
2026-03-21 21:45:43 +0100

    Angular Hero Page V1

--- portfolio-frontend/src/app/components/hero/hero.component.ts
+++ portfolio-frontend/src/app/components/hero/hero.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
